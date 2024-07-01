cfcf68834fae8979730318012ba412a1ab24da8b
jolienguyen28 <123792049+jolienguyen28@users.noreply.github.com>
2024-07-02 00:43:52 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var audio1 = document.getElementById('audio1');
+    var audio2 = document.getElementById('audio2');
+  
+    // Play audio1 when playAudio1 button is clicked
+    document.getElementById('playAudio1').addEventListener('click', function() {
+      audio1.play();
+    });
+  
+    // Play audio2 when playAudio2 button is clicked
+    document.getElementById('playAudio2').addEventListener('click', function() {
+      audio2.play();
+    });
+  });
+document.addEventListener('DOMContentLoaded', function() {
+  var audio1 = document.getElementById('audio1');
+  var audio2 = document.getElementById('audio2');
+
+  // Play audio1 when playAudio1 button is clicked
+  document.getElementById('playAudio1').addEventListener('click', function() {
+    audio1.play();
+  });
+
+  // Play audio2 when playAudio2 button is clicked
+  document.getElementById('playAudio2').addEventListener('click', function() {
+    audio2.play();
+  });
+});
+  
